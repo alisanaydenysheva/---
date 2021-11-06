d5d7720fbb6892fe64b964c0e0d2e05061d653c9
alisanaydenysheva <93647369+alisanaydenysheva@users.noreply.github.com>
2021-11-06 15:27:37 +0300

    Create contour.cpp

---
+++ OpenCV/2/contour.cpp
@@ -0,0 +1,47 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace cv;
+using namespace std;
+
+Mat img;
+Mat src_gray;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char filename[80]; //
+	cout << "Введите имя файла, в который хотите внести изменения, и нажмите Enter" << endl;
+	cin.getline(filename, 80);
+	cout << "Открыт файл";
+	cout << filename << endl;
+
+	Mat img = imread(filename, 1);
+	namedWindow("Исходное изображение", WINDOW_AUTOSIZE);
+	imshow("Исходное изображение", img);
+
+
+
+	Mat canny_output;
+	cvtColor(img, src_gray, COLOR_RGB2GRAY);
+	blur(src_gray, src_gray, Size(3, 3));
+
+	double otsu_thresh_val = threshold(src_gray, img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	double high_thresh_val = otsu_thresh_val, lower_thresh_val = otsu_thresh_val * 0.5;
+	cout << otsu_thresh_val;
+	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3);
+
+	namedWindow("Серое изображение", WINDOW_AUTOSIZE);
+	imshow("Серое изображение", canny_output);
+	imwrite("canny_output.jpg", canny_output);
+
+
+
+
+	waitKey(0);
+	return 0;
+}
